5c10fb79c24318b66fcf0bd710a32d52153a9177
Aleksei Morozov <aleksei_morozov@epam.com>
2022-02-04 13:41:57 +0400

    initial commit

---
+++ kafka-listener/src/test/java/com/epam/amorozov/kafka/listener/KafkaListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.amorozov.kafka.listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ kafka-producer/src/main/java/com/epam/amorozov/kafka/producer/KafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.epam.amorozov.kafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class, args);
+    }
+
+}
